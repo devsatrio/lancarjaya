0e11b55ea83eb1e0807699c13bf4cfa0aa46215c
notbeable <blackwolf2103@outlook.com>
2020-01-23 15:24:30 +0700

    logo

--- contact/apps.py
+++ contact/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ContactConfig(AppConfig):
     name = 'contact'
+    icon_name = 'account_circle'
---
+++ galeri/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'galeri.apps.GaleriConfig'
