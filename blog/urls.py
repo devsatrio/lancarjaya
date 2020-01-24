909d178f58f951934db2ee3a18d02aa3f20a3ad8
zicom99 <50070374+zicom99@users.noreply.github.com>
2020-01-24 15:23:35 +0700

    update aku boss

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name='blog'
+
 urlpatterns = [
-    
+    path('kategori/<int:kategori>',views.bykategori,name='kategori'),
+    path('<slug:blogslug>',views.show,name='show'),
+    path('',views.index,name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from . import models
+from django.core.paginator import Paginator
 
-# Create your views here.
+def index(request):
+    data_kategori = models.kategori.objects.all()
+    data_artikel = models.artikel.objects.all().order_by('-id')
+    paginator = Paginator(data_artikel, 5)
+    page = request.GET.get('page')
+    data_artikel = paginator.get_page(page)
+
+    #parsing data
+    context = {
+        'kategori':data_kategori,
+        'artikel':data_artikel,
+    }
+    return render(request, 'blog/index.html',context)
+
+def show(request,blogslug):
+    detail_artikel = models.artikel.objects.get(slug=blogslug)
+    artikel_lain = models.artikel.objects.all().order_by('?')[0:3]
+    context = {
+        'detail_artikel':detail_artikel,
+        'artikel_lain':artikel_lain,
+    }
+    return render(request,'blog/show.html',context)
+
+def bykategori(request,kategori):
+    data_kategori = models.kategori.objects.all()
+    data_artikel = models.artikel.objects.all().filter(kategori=kategori).order_by('-id')
+    paginator = Paginator(data_artikel, 3)
+    page = request.GET.get('page')
+    data_artikel = paginator.get_page(page)
+    detail_kategori = models.kategori.objects.get(id=kategori)
+    context = {
+        'detail_kategori':detail_kategori,
+        'kategori':data_kategori,
+        'artikel':data_artikel,
+    }
+    return render(request,'blog/kategori.html',context)
+
+
+
+
