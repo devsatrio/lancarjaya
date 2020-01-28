d7319bf39cd10ef1a83d57b44e9c008955277be2
devasatrio <34015427+devsatrio@users.noreply.github.com>
2020-01-28 10:00:25 +0700

    update home

--- testimoni/views.py
+++ testimoni/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from .forms import testimoniform
+from .models import testi
 def index(request):
+    if request.method == 'POST':
+        testi.objects.create(
+            nama=request.POST['nama'],
+            email = request.POST['email'],
+            perihal = request.POST['perihal'],
+            deskripsi = request.POST['deskripsi'],
+        )
+        messages.success(request,'Makasih Atas Penilaianya')
+        return redirect('testimoni:index')
+    
+    data_testi = testi.objects.all().order_by('-id')
     context = {
         'testimoniform':testimoniform(),
-        'head':'halo',
+        'testi':data_testi,
     }
     return render(request,'testimoni/index.html',context)
