22c1a22aaf4656fee572148a555be8f4b2b6c04f
zicom99 <50070374+zicom99@users.noreply.github.com>
2020-01-28 11:48:25 +0700

    update dulu

--- lancarjaya/views.py
+++ lancarjaya/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from testimoni.models import testi
 from produk.models import barang
+from blog.models import artikel
+
 def index(request):
     data_testi = testi.objects.all().order_by('-id')[0:4]
     data_barang = barang.objects.all().order_by('-id')[0:8]
+    data_artikel = artikel.objects.all().order_by('-id')[0:3]
+    
+    #parsing data
     context = {
         'testi':data_testi,
         'produk':data_barang,
+        'artikel':data_artikel,
     }
     return render(request,'index.html',context)
