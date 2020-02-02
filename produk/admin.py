5103324523b9820ee680bde9cb00321173619a8b
devasatrio <34015427+devsatrio@users.noreply.github.com>
2020-02-02 19:40:45 +0700

    update blog

--- produk/admin.py
+++ produk/admin.py
@@ -3,13 +3,13 @@ from . import models
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-class ProdukResource(resources.ModelResource):
+# class ProdukResource(resources.ModelResource):
 
-	class Meta:
-		model = models.kategori
-		fields = ('id','nama','tanggal_buat')
+# 	class Meta:
+# 		model = models.kategori
+# 		fields = ('id','nama','tanggal_buat')
 
-class roleskategori(ImportExportModelAdmin):
+class roleskategori(admin.ModelAdmin):
     list_display = ('nama','tanggal_buat')
     list_filter = ('tanggal_buat',)
     readonly_fields = ('tanggal_buat','slug')
