3ebd88255b772a6371b7957871f3c3234c5a813f
deva <satriosuklun@gmail.com>
2020-02-09 20:08:35 +0700

    update keranjang belanja

--- transaksi/views.py
+++ transaksi/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import keranjang
+from produk.models import barang
+from django.db.models import Avg
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required
+def listkeranjang(request,kodeuser):
+    data_keranjang = keranjang.objects.filter(pembeli=User.objects.get(username=kodeuser))
+    subtotal = keranjang.objects.filter(pembeli=User.objects.get(username=kodeuser)).aggregate(Avg('barang'))
+    context = {
+        'barang':data_keranjang,
+        'subtotal':subtotal,
+    }
+    return render(request,'transaksi/cart.html',context)
+
+@login_required
+def hapuskeranjang(request,kodeuser):
+    if request.method == 'POST':
+        data = keranjang.objects.get(id=request.POST['kode'])
+        data.delete()
+        messages.success(request,'Berhasil menghapus barang dari keranjang')
+        return redirect('transaksi:keranjang',kodeuser=kodeuser)
+    
+
+    data_keranjang = keranjang.objects.filter(pembeli=User.objects.get(username=kodeuser))
+    context = {
+        'barang':data_keranjang,
+    }
+    return render(request,'transaksi/cart.html',context)
