0b1ac1bb35e85d405fce643f7e941dca86d38363
deva <satriosuklun@gmail.com>
2020-02-11 18:31:33 +0700

    update
    update

--- produk/migrations/0002_auto_20200207_0946.py
+++ produk/migrations/0002_auto_20200207_0946.py
@@ -10,34 +10,34 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='barang',
-            name='gambar',
-            field=models.ImageField(blank=True, null=True, upload_to='produk/'),
-        ),
-        migrations.AddField(
-            model_name='barang',
-            name='slug',
-            field=models.SlugField(editable=False, null=True),
-        ),
-        migrations.AddField(
-            model_name='barang',
-            name='status_aktiv',
-            field=models.BooleanField(default=False),
-        ),
-        migrations.AddField(
-            model_name='kategori',
-            name='slug',
-            field=models.SlugField(editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name='barang',
-            name='nama',
-            field=models.CharField(max_length=50, unique=True),
-        ),
-        migrations.AlterField(
-            model_name='kategori',
-            name='nama',
-            field=models.CharField(max_length=50, unique=True),
-        ),
+        # migrations.AddField(
+        #     model_name='barang',
+        #     name='gambar',
+        #     field=models.ImageField(blank=True, null=True, upload_to='produk/'),
+        # ),
+        # migrations.AddField(
+        #     model_name='barang',
+        #     name='slug',
+        #     field=models.SlugField(editable=False, null=True),
+        # ),
+        # migrations.AddField(
+        #     model_name='barang',
+        #     name='status_aktiv',
+        #     field=models.BooleanField(default=False),
+        # ),
+        # migrations.AddField(
+        #     model_name='kategori',
+        #     name='slug',
+        #     field=models.SlugField(editable=False, null=True),
+        # ),
+        # migrations.AlterField(
+        #     model_name='barang',
+        #     name='nama',
+        #     field=models.CharField(max_length=50, unique=True),
+        # ),
+        # migrations.AlterField(
+        #     model_name='kategori',
+        #     name='nama',
+        #     field=models.CharField(max_length=50, unique=True),
+        # ),
     ]
