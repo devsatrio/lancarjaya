63bbdf89e545527de829524879fd41750a668aa5
deva <satriosuklun@gmail.com>
2020-02-14 23:22:01 +0700

    update transaksi

--- transaksi/admin.py
+++ transaksi/admin.py
@@ -7,3 +7,8 @@ class rolestransaksi(admin.ModelAdmin):
     list_display = ('pembeli','barang','tanggal_buat','berat_total','jumlah','total')
 
 admin.site.register(models.keranjang,rolestransaksi)
+
+class rolestransaksis(admin.ModelAdmin):
+    list_display = ('pembeli','tanggal_beli','opsi_pengiriman')
+
+admin.site.register(models.transaksi,rolestransaksis)
